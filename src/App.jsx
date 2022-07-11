0e661b594c0a70d5de0808c94d7f882647993635
taroosg <root@taroosg.dev>
2022-07-11 16:04:15 +0900

    add cd

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { ActionButton } from "./components/ActionButton";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Omikuji } from "./pages/Omikuji";
 import { Janken } from "./pages/Janken";
