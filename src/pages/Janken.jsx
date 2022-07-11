7bc6d0fb3f12268c7aa4a3e57f5400d3e05a227a
taroosg <root@taroosg.dev>
2022-07-11 10:55:40 +0900

    update pages

--- src/pages/Janken.jsx
+++ src/pages/Janken.jsx
@@ -1,7 +1,12 @@
+import { ActionButton } from "../components/ActionButton.jsx";
+
 export const Janken = () => {
   return (
     <>
       <p>じゃんけんの画面</p>
+      <ActionButton
+        text="じゃんけんをする"
+      />
     </>
   )
 }
--- src/pages/Omikuji.jsx
+++ src/pages/Omikuji.jsx
@@ -1,7 +1,12 @@
+import { ActionButton } from "../components/ActionButton.jsx";
+
 export const Omikuji = () => {
   return (
     <>
       <p>おみくじの画面</p>
+      <ActionButton
+        text="おみくじをひく"
+      />
     </>
   )
 }
